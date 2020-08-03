10908dd69b362a8e1bcc57cbbaa74113cb4b7b06
sellnat77 <sell_nat@yahoo.com>
2020-08-02 18:43:55 -0700

    Cleaning up some readability and utilized tempfiles

--- Dumstigram/Api/app.py
+++ Dumstigram/Api/app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory
+from flask import Flask, request, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
+from Filters import laserEyes
 app = Flask(__name__)
 app.config.from_pyfile('default.default_settings')
 app.config.from_envvar('DAPI_ENV_OVERRIDE', silent=True)
@@ -38,15 +39,20 @@ def upload_file():
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        dest_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(dest_file)
+
+        # choose random filter and apply it here
+        filterClass = laserEyes.laserEyes()
+        filtered_image = filterClass.apply_filter(dest_file)
+
         return redirect(url_for('uploaded_file',
-                                filename=filename))
+                                filename=filtered_image))
 
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_file(filename)
 
 
 if __name__ == '__main__':
