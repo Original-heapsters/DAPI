24d2289ea4c2178ba95b431fe8e687ae928a73e9
Andrew <hellaandrew@gmail.com>
2021-10-30 22:55:26 -0700

    Added Android project

---
+++ Dumstigram/Android/app/src/main/java/com/ogheapsters/dumstigram/ui/main/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.ogheapsters.dumstigram.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ogheapsters.dumstigram.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
