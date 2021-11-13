718cf2a0e15d6c693d2ac082691504d9385f2968
sellnat77 <sell_nat@yahoo.com>
2021-11-13 02:24:34 -0800

    Added simplistic frontend

--- Dumstigram/Api/app.py
+++ Dumstigram/Api/app.py
@@ -1,7 +1,8 @@
 import os
 import io
 import random
-from flask import Flask, request, redirect, url_for, send_file
+import uuid
+from flask import Flask, request, redirect, url_for, send_file, render_template
 from werkzeug.utils import secure_filename
 from Filters import laserEyes, noise, brightnessContrast, bulge, inpaint
 import redis
@@ -23,6 +24,10 @@ def hello():
 def swag():
     return "69 BROOOOO!"
 
+
+@app.route('/home')
+def upload_form():
+	return render_template('upload.html')
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -65,8 +70,44 @@ def upload_file():
             attachment_filename=filtered_image
         )
 
-        return redirect(url_for('uploaded_file',
-                                filename=filtered_image))
+
+@app.route('/home', methods=['POST'])
+def upload_file_testing():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return 'No file was uploaded'
+
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        return 'No selected file'
+
+    if file and allowed_file(file.filename):
+        file_components = file.filename.rsplit('.', 1)
+        name = uuid.uuid4().hex
+        filename = secure_filename(name + '.' + file_components[-1])
+        dest_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(dest_file)
+        app.logger.debug(f'Saving temp file to {dest_file}')
+
+        # choose random filter and apply it here
+        filter_classes = [laserEyes.laserEyes(), noise.noise(), brightnessContrast.brightnessContrast(), bulge.bulge(), inpaint.inpaint()]
+        filterClass = random.choice(filter_classes)
+        filtered_image = filterClass.apply_filter(dest_file)
+        os.replace(filtered_image, dest_file)
+        with open(dest_file, 'rb') as f:
+            s = f.read()
+            r.setex('test', 30, s)
+            app.logger.debug('Saving temp file to redis key test')
+
+        return render_template('upload.html', filename=filename)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    print(filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
 @app.route('/uploads/<filename>')
