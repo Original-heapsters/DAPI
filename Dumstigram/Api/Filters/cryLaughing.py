e03fb85329a5aaa0fe0a0aa742abffe1a2602ee8
sellnat77 <sell_nat@yahoo.com>
2022-02-13 11:04:39 -0800

    Added cry laughing overlay

--- Dumstigram/Api/Filters/cryLaughing.py
+++ Dumstigram/Api/Filters/cryLaughing.py
@@ -19,31 +19,45 @@ class cryLaughing(object):
                 }
 
     def filter_image(self, input):
-        overlay = cv2.imread(self.additives + '/cry_laughing.png', -1)
-        height, width, channels = input.shape
-        overlay = cv2.resize(overlay, (width, height), interpolation=cv2.INTER_AREA)
 
-        print(input.shape)
-        print(overlay.shape)
+        emoji_chosen = 'cry_laughing'
+        overlay = cv2.imread(self.additives + '/' + emoji_chosen + '.png', -1)
 
-        # def resize_overlay(dest_width, overlay):
-        #     ratio = dest_width / overlay.shape[1]
-        #     dimension = (dest_width, int(overlay.shape[0] * ratio))
-        #     return cv2.resize(overlay, dimension, interpolation=cv2.INTER_AREA)
-        #
-        # def overlay_with_alpha(x1, y1, x2, y2, overlay, background):
-        #     alpha_over = overlay[:, :, 3] / 255.0
-        #     alpha_bg = 1.0 - alpha_over
-        #
-        #     for c in range(0, 3):
-        #         background[y1:y2, x1:x2, c] = (alpha_over * overlay[:, :, c] +
-        #                                        alpha_bg * background[y1:y2,
-        #                                        x1:x2,
-        #                                        c])
-        #     return background
+        def resize_overlay(dest_width, overlay):
+            ratio = dest_width / overlay.shape[1]
+            dimension = (dest_width, int(overlay.shape[0] * ratio))
+            return cv2.resize(overlay, dimension, interpolation=cv2.INTER_AREA)
 
-        added_image = cv2.addWeighted(input, 0.3, overlay, 0.7, 0.0)
-        return added_image
+        def calc_position(x, y, overlay):
+            # Define bounding diagonal points for overlay
+            y1, y2 = y, y + overlay.shape[0]
+            x1, x2 = x, x + overlay.shape[1]
+            return x1, y1, x2, y2
+
+        def overlay_with_alpha(x1, y1, x2, y2, overlay, background):
+            alpha_over = overlay[:, :, 3] / 255.0
+            alpha_bg = 1.0 - alpha_over
+
+            for c in range(0, 3):
+                background[y1:y2, x1:x2, c] = (alpha_over * overlay[:, :, c] +
+                                               alpha_bg * background[y1:y2,
+                                               x1:x2,
+                                               c])
+            return background
+
+        min_width = int(input.shape[0] / 8)
+        max_width = int(input.shape[0] / 2)
+        target_width = random.randint(min_width, max_width)
+        resized = resize_overlay(target_width, overlay)
+
+        # X: 0 -> anywhere within the width (minus width of overlay)
+        # Y: 0 -> anywhere within the height (minus height of *resized overlay)
+        target_x = random.randint(0, input.shape[1] - target_width)
+        target_y = random.randint(0, input.shape[0] - resized.shape[0])
+
+        x1, y1, x2, y2 = calc_position(target_x, target_y, resized)
+        input = overlay_with_alpha(x1, y1, x2, y2, resized, input)
+        return input
 
     def identify_prep(self, input):
         return None
