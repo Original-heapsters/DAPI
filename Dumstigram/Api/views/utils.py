8d6c5bdc106cf6301da796377ecbc6d41a8082d6
sellnat77 <sell_nat@yahoo.com>
2022-03-06 20:36:07 -0800

    Adds discord bot code and updates api to accept file urls

--- Dumstigram/Api/views/utils.py
+++ Dumstigram/Api/views/utils.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import random
+import requests
 import uuid
 from flask import current_app, request
 from werkzeug.utils import secure_filename
@@ -43,14 +44,7 @@ def apply_random_filters(filters, input_img):
     return running_img
 
 
-def process_image(filter_name=None):
-    # Clear out static folder to preserve space
-    clear_dir(uploads)
-
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        return 'No file was uploaded'
-
+def use_given_file(filter_name):
     file = request.files['file']
     # if user does not select file, browser also
     # submit an empty part without filename
@@ -64,21 +58,49 @@ def process_image(filter_name=None):
         input_file = os.path.join(uploads, filename)
         file.save(input_file)
         logger.debug(f'Saving temp file to {input_file}')
+        return apply_filters(filter_name, input_file, filename, name)
 
-        if filter_name and filter_classes[filter_name]:
-            chosen_filter = filter_classes[filter_name]
-            filtered_img = apply_random_filters([chosen_filter], input_file)
-        else:
-            possible_filters = list(filter_classes.values())
-            filtered_img = apply_random_filters(possible_filters, input_file)
-
-        # Overwrite uploaded img with filtered version
-        if filtered_img is not None:
-            shutil.move(filtered_img, input_file)
-        with open(input_file, 'rb') as f:
-            file_bytes = f.read()
-            redis.setex(name, current_app.config['REDIS_TTL'], file_bytes)
-            logger.debug(f'Saving filtered img bytes to redis key {name}')
-
-        return filename, name
     return f'File not allowed {file.filename}'
+
+
+def use_file_url(filter_name):
+    file_request = requests.get(request.args.get('file_url'))
+
+    name = uuid.uuid4().hex
+    filename = secure_filename(name + '.png')
+    input_file = os.path.join(uploads, filename)
+    with open(input_file, "wb") as f:
+        f.write(file_request.content)
+        f.close()
+    return apply_filters(filter_name, input_file, filename, name)
+
+
+def apply_filters(filter_name, input_file, filename, name):
+    if filter_name and filter_classes[filter_name]:
+        chosen_filter = filter_classes[filter_name]
+        filtered_img = apply_random_filters([chosen_filter], input_file)
+    else:
+        possible_filters = list(filter_classes.values())
+        filtered_img = apply_random_filters(possible_filters, input_file)
+
+    # Overwrite uploaded img with filtered version
+    if filtered_img is not None:
+        shutil.move(filtered_img, input_file)
+    with open(input_file, 'rb') as f:
+        file_bytes = f.read()
+        redis.setex(name, current_app.config['REDIS_TTL'], file_bytes)
+        logger.debug(f'Saving filtered img bytes to redis key {name}')
+
+    return filename, name
+
+
+def process_image(filter_name=None):
+    # Clear out static folder to preserve space
+    clear_dir(uploads)
+
+    if 'file_url' in request.args:
+        return use_file_url(filter_name)
+    elif 'file' in request.files:
+        return use_given_file(filter_name)
+    else:
+        return 'No file was uploaded or no url was provided'
