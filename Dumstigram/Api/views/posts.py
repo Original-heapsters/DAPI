2348c7877f5cdb2515f19ff0aaea3d7954fb9ba1
sellnat77 <sell_nat@yahoo.com>
2022-04-12 22:45:36 -0700

    Fix github action file glob for backend

--- Dumstigram/Api/views/posts.py
+++ Dumstigram/Api/views/posts.py
@@ -120,23 +120,3 @@ def users_posts(id):
 @posts.route('')
 def upload_form():
     return render_template('upload.html')
-
-
-# @posts.route('', methods=['POST'])
-# def upload_file():
-#     asApi = request.args.get('asApi')
-#     print('asApi is {}'.format(asApi))
-#     result = utils.process_image()
-#     filename, name = result
-#     if asApi and name:
-#         # send file as attachment as api response
-#         return send_file(
-#             io.BytesIO(redis.get(name)),
-#             as_attachment=True,
-#             attachment_filename=filename
-#         )
-#     elif not asApi and filename:
-#         # re-render to barebones frontend
-#         return render_template('upload.html', filename=filename)
-#     else:
-#         return result
